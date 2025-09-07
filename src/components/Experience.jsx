0a1bd0716fdab36644a5455a805a0b1516171f5a
Lluancet <103333263+Lluan12@users.noreply.github.com>
2025-09-07 08:54:14 -0700

    Update Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -2,10 +2,10 @@ import { ExperienceItem } from "../components/ExperienceItem";
 
 const EXPERIENCE = [
 	{
-		date: "Marzo 2024 - Septiembre 2024",
+		date: "Mayo 2024 - Noviembre 2024",
 		title: "Desarrollador FullStack - Instituto Tecnológico de Tepic",
 		description:
-			"Desarrollé un módulo funciona para la plataforma de MiTec, utilizado por estudiantes y docentes del Instituto Tecnológico de Tepic para gestionar sesiones de asesoría académica.",
+			"Desarrollé un módulo funcional para la plataforma de MiTec, utilizado por estudiantes y docentes del Instituto Tecnológico de Tepic para gestionar sesiones de asesoría académica.",
 		link: "",
 	},
 ];
